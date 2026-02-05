fd41a7e6185bd68a57000c81b1776caf3e565bc7
OBD <organizacaobrasileiradedardos@gmail.com>
2026-02-04 21:01:51 -0300

    Fix syntax error in settings.py (dj_database_url import)

--- OBD-master/obd/settings.py
+++ OBD-master/obd/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config, Csv
-from dj_database_url
+import dj_database_url
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
