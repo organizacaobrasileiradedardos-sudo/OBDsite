1d769b59e78bc4390ed1b9faacfb96e158fd208e
OBD <organizacaobrasileiradedardos@gmail.com>
2026-02-04 21:47:51 -0300

    fix: align Cloudinary env var with settings

--- OBD-master/obd/settings.py
+++ OBD-master/obd/settings.py
@@ -170,7 +170,7 @@ EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool)
 
 cloudinary.config(
     cloud_name=config('CLOUDINARY_NAME'),
-    api_key=config('CLOUDINARY_API'),
+    api_key=config('CLOUDINARY_API_KEY'),
     api_secret=config('CLOUDINARY_API_SECRET'),
     secure=config('CLOUDINARY_SECURE')
 )
