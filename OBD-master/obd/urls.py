88e6fb24bb8c8f6594cdd4878a90329de5528a85
OBD <organizacaobrasileiradedardos@gmail.com>
2026-02-12 19:24:37 -0300

    Alteração no urls

--- OBD-master/obd/urls.py
+++ OBD-master/obd/urls.py
@@ -1,46 +1,46 @@
-"""obd URL Configuration
-
+"""
+obd URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    1. Add an import: from my_app import views
+    2. Add a URL to urlpatterns: path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    1. Add an import: from other_app.views import Home
+    2. Add a URL to urlpatterns: path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns: path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
-from django.contrib.staticfiles.storage import staticfiles_storage
-import obd.core.views
+from django.conf import settings
+from django.conf.urls.static import static
+from obd.core.views import index  # Importe explicitamente para clareza
 from obd.subscriptions.views import subscribe
-from obd.dashboards.administrators import urls
-from obd.dashboards.administrators.enviroments import urls
-from obd.dashboards.administrators.leagues import urls
-from obd.dashboards.players import urls
-from obd.dashboards.players.profiles import urls
-from obd.dashboards.administrators.fixtures import urls
-from obd.dashboards.administrators.results import urls
-from obd.dashboards.administrators.champions import urls
-
+from obd.dashboards.administrators import urls as admin_urls
+from obd.dashboards.administrators.enviroments import urls as enviroments_urls
+from obd.dashboards.administrators.leagues import urls as leagues_urls
+from obd.dashboards.players import urls as players_urls
+from obd.dashboards.players.profiles import urls as profiles_urls
+from obd.dashboards.administrators.fixtures import urls as fixtures_urls
+from obd.dashboards.administrators.results import urls as results_urls
+from obd.dashboards.administrators.champions import urls as champions_urls
 
 urlpatterns = [
-    path('', obd.core.views.index, name='index'),
-    path('', include(obd.dashboards.players.urls, namespace='players')),
-    path('', include(obd.dashboards.administrators.urls, namespace='administrators')),
-    path('', include(obd.dashboards.administrators.enviroments.urls, namespace='enviroments')),
-    path('', include(obd.dashboards.players.profiles.urls, namespace='profiles')),
-    path('', include(obd.dashboards.administrators.leagues.urls, namespace='league')),
-    path('', include(obd.dashboards.administrators.fixtures.urls, namespace='fixtures')),
-    path('', include(obd.dashboards.administrators.results.urls, namespace='results')),
-    path('', include(obd.dashboards.administrators.champions.urls, namespace='champions')),
-    path('subscribe/', subscribe),
-    path('admin/', admin.site.urls),
+    path('', index, name='index'),  # Raiz do site - confirme se 'index' existe em core/views.py
+    path('dashboard/players/', include(players_urls, namespace='players')),  # Prefixo para evitar conflitos
+    path('dashboard/administrators/', include(admin_urls, namespace='administrators')),
+    path('dashboard/enviroments/', include(enviroments_urls, namespace='enviroments')),
+    path('dashboard/profiles/', include(profiles_urls, namespace='profiles')),
+    path('dashboard/leagues/', include(leagues_urls, namespace='league')),
+    path('dashboard/fixtures/', include(fixtures_urls, namespace='fixtures')),
+    path('dashboard/results/', include(results_urls, namespace='results')),
+    path('dashboard/champions/', include(champions_urls, namespace='champions')),
+    path('subscribe/', subscribe),  # Removida duplicata
+    path('admin/', admin.site.urls),  # Removida duplicata
     path('boa/players/', obd.core.views.public_players, name='boaplayers'),
     path('boa/leagues/', obd.core.views.public_leagues, name='boaleagues'),
     path('boa/leagues/<slug:slug>/details', obd.core.views.public_league_view, name='boaleagueview'),
@@ -51,12 +51,9 @@ urlpatterns = [
     path('noticias/', obd.core.views.news_list, name='news'),
     path('documentos/', obd.core.views.documents_list, name='documents'),
     path('obd/', obd.core.views.obd_organization, name='obd_organization'),
-    path('favicon.ico', RedirectView.as_view(url='/static/media/logos/LOGO_OBD.png')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('media/logos/LOGO_OBD.png'))),  # Usando staticfiles_storage para melhor compatibilidade
 ]
 
 # Serve media files in development
-from django.conf import settings
-from django.conf.urls.static import static
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
