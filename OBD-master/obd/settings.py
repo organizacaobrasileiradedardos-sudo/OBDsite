d8696278ab9efef8cc845529322c5f59c73cdb2e
OBD <organizacaobrasileiradedardos@gmail.com>
2026-02-15 22:05:27 -0300

    Change DEBUG to FALSE

--- OBD-master/obd/settings.py
+++ OBD-master/obd/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True             # config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ['*']    # ["obdsite-production.up.railway.app", ".up.railway.app", "localhost", "127.0.0.1"]
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 LOGIN_URL = '/dashboard/player/login'
