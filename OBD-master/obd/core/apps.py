7d94e93b11acd7b78171bb926cf64dc4d62671a3
organizacaobrasileiradedardos-sudo <organizacaobrasileiradedardos@gmail.com>
2026-02-16 16:20:30 -0300

    Update apps.py first time

--- OBD-master/obd/core/apps.py
+++ OBD-master/obd/core/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+from pathlib import Path
 
 
 class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'obd.core'
+    path = Path(__file__).resolve().parent
