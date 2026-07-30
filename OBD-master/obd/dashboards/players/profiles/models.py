b35f90d8c56ee145b8f9d39b6b7d1a61f9945a1e
organizacaobrasileiradedardos-sudo <organizacaobrasileiradedardos@gmail.com>
2026-07-29 22:17:34 -0300

    Update models.py

--- OBD-master/obd/dashboards/players/profiles/models.py
+++ OBD-master/obd/dashboards/players/profiles/models.py
@@ -19,7 +19,7 @@ class Profile(models.Model):
     nickname = models.CharField(max_length=40, null=False, blank=True)
     photo = models.ImageField(default='/static/media/logos/Logo_OBD_3.jpeg')
     birth_date = models.DateField(blank=True, null=True)
-    pin = models.CharField(max_length=10, null=False, blank=True)
+    pin = models.CharField(max_length=50, null=False, blank=True)
     slug = models.CharField(max_length=180, null=False, blank=True)
     bio = models.TextField(max_length=250, null=False, blank=True)
     country = models.CharField(max_length=60, blank=True, default='Brasil')
